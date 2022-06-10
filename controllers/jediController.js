d789c7f1b82c7d9b67e8154abbcf5f8cdebaef7e
Ori Marom <ori.marom10@gmail.com>
2022-06-10 12:38:03 +0300

    todo 1-4

--- controllers/jediController.js
+++ controllers/jediController.js
@@ -1,69 +1,67 @@
 const jediService = require("../service/JediService");
 
 async function createJedi(req, res) {
-    console.log("saving jedi");
-    await jediService.addJedi(req.body);
-    res.status(200).json(req.body);
+  console.log("saving jedi");
+  await jediService.addJedi(req.body);
+  res.status(200).json(req.body);
 }
 
-
 async function getJedi(req, res) {
-    let jediId = Number.parseInt(req.params.id);
-    //TODO 1. Turn it to error with proper status and throw it
-    if (isNaN(jediId)) return res.status(400).json({
-        status: 400,
-        error: "wrong parameters"
-    });
+  let jediId = Number.parseInt(req.params.id);
+  //TODO 1. Turn it to error with proper status and throw it
+  let error = Error("Jedi must be number");
+  if (isNaN(jediId)) {
+    error.statusCode = 400;
+    throw error;
+  }
 
-    const jedi = await jediService.getJedi(jediId);
+  const jedi = await jediService.getJedi(jediId);
+  let error = Error("This is not a Jedi");
+  if (!jedi) {
+    //TODO 2. Turn it to error with proper status and throw it
+    error.status = 404;
+    throw error;
+  }
 
-    if (!jedi) {
-        //TODO 2. Turn it to error with proper status and throw it
-        return res.status(404).json({
-            status: 404,
-            error: "Not found"
-        });
-    }
-
-    res.status(200).json(jedi);
+  res.status(200).json(jedi);
 }
 
 async function getAll(req, res) {
-    let data = await jediService.getAll();
-    if (!data) data = [];
-    res.status(200).json(data);
+  let data = await jediService.getAll();
+  if (!data) data = [];
+  res.status(200).json(data);
 }
 
 async function replaceJedi(req, res) {
-    const jediId = Number.parseInt(req.params.id);
-    if (isNaN(jediId)) {
-        //TODO 3. Turn it to error with proper status and throw it
-        return res.status(400).json({
-            status: 400,
-            error: "wrong parameters"
-        });
-    }
+  const jediId = Number.parseInt(req.params.id);
+  let error = Error("Jedi must be number");
+  if (isNaN(jediId)) {
+    //TODO 3. Turn it to error with proper status and throw it
+    error.statusCode = 400;
+    throw error;
+  }
 
-    const data = await jediService.replaceJedi(jediId, req.body);
-    res.status(200).json(data);
+  const data = await jediService.replaceJedi(jediId, req.body);
+  res.status(200).json(data);
 }
 
 async function deleteJedi(req, res) {
-    let jediId = Number.parseInt(req.params.id);
-    //TODO 4. Turn it to error with proper status and throw it
-    if (isNaN(jediId)) return res.status(400).json({
-        status: 400,
-        error: "wrong parameters"
-    });
+  let jediId = Number.parseInt(req.params.id);
+  let error = Error("Jedi must be number");
+  //TODO 4. Turn it to error with proper status and throw it
+  if (isNaN(jediId)){
+  error.statusCode = 400;
+  throw error;
+  }
 
-    const data = await jediService.deleteJedi(jediId);
-    res.status(200).json(data);
+  const data = await jediService.deleteJedi(jediId);
+  res.status(200).json(data);
 }
 
 module.exports = {
-    createJedi,
-    deleteJedi,
-    getAll,
-    getJedi,
-    replaceJedi
+  createJedi,
+  deleteJedi,
+  getAll,
+  getJedi,
+  replaceJedi,
 };
